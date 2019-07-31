bc3e21153facf34685c7665941074e3be96f6378
Matheus Penchel <matheus.penchel@gmail.com>
2019-07-30 22:10:27 -0300

    Added more tests; renamed until_christmas -> days_until_christmas

--- xmas.rb
+++ xmas.rb
@@ -1,6 +1,6 @@
 require 'date'
 
-def until_christmas(year=Date.today.year)
+def days_until_christmas(year=Date.today.year)
   christmas_date = Date.new(year, 12, 25)
 
   if Date.today > christmas_date
@@ -11,6 +11,22 @@ def until_christmas(year=Date.today.year)
 end
 
 
-puts until_christmas.class == Integer
-puts until_christmas == 148
-puts until_christmas
+puts days_until_christmas.class == Integer
+
+# For this to keep working in the future
+time_diff = (Date.today - Date.new(2019, 7, 30)).to_i
+
+# Considering when this was done, on July 30, 2019
+puts days_until_christmas == 148 + time_diff
+puts days_until_christmas(2020) == 514 + time_diff
+
+# We're traveling to the future, folks!
+# To test the post-xmas condition.
+class Date
+  def Date.today
+    return Date.new(2019,12,27)
+  end
+end
+
+# 27 December 2019 -> xmas 2020
+puts days_until_christmas == 364
